f68749abe99fa5d204f348cb11586e7a24d69cbe
Amit Reinich <amitreinich@gmail.com>
2021-02-14 18:56:34 +0200

    start frontside

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,28 @@
+import "../styles/landingPage.scss";
+import googleIcon from "../assets/google-icon.svg";
+import landingImage from "../assets/landing-image.svg";
+const LandingPage = () => {
+  return (
+    <div className="ladingPage-body">
+      <div className="navBar">
+        <h1 className="logo">Roomates</h1>
+        <a className="login-btn">LOGIN</a>
+      </div>
+      <div className="content-container">
+        <div className="main-content-wrapper">
+          <div className="main-text">
+            <h2>Manage your apartment expenses in a smart & maintainable way</h2>
+            <h3>• Keep track of all yours shared expenses around the apartment.</h3>
+            <h3>• Easy to understand and maintain dashboard with bunch of features.</h3>
+            <a className="google-login">
+              <img src={googleIcon} /> Start now
+            </a>
+          </div>
+          <img className="bg-image" src={landingImage} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
